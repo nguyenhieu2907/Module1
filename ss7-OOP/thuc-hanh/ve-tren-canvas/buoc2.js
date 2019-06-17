fc5c5453cba61ebee607a93d2e5d2f38de633abb
nguyenhieu2907 <med290796@gmail.com>
2019-06-18 00:18:55 +0700

    ok

---
+++ ss7-OOP/thuc-hanh/ve-tren-canvas/buoc2.js
@@ -0,0 +1,13 @@
+function Circle(x, y, radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+ }
+ function createCircle(){
+     var ctx = document.getElementById("myCanvas").getContext("2d");
+     var circle= new Circle(500, 500, 80);
+     ctx.beginPath();
+     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+     ctx.fill();
+ }
+ createCircle();
---
+++ ss7-OOP/thuc-hanh/ve-tren-canvas/buoc3.js
@@ -0,0 +1,15 @@
+function Circle(x, y, radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+ }
+ function createCircle(){
+     var ctx = document.getElementById("myCanvas").getContext("2d");
+     var radius = Math.floor(Math.random() * 80);
+     var circle= new Circle(500, 500, radius);
+     ctx.beginPath();
+     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+     ctx.fill();
+ }
+ 
+ createCircle();
